45bc73f95483b01912d05e88123f871e8cfcbf6c
Zzz <a25zihzhazha@inspedralbes.cat>
2026-03-13 09:29:13 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# AEA5.2.-Desenvolupament-d-un-CRUD-en-PHP-MySQLi-en-servidor-real
+Este ejercicio he realizar las cuatro operaciones fundamentales en una 
+base de datos utilizando PHP y  MySQLi.
+
+Al terminar, veremos de forma práctica cómo insertar, leer, modificar y borrar información. 
+Y también incorpora Bootstrap como framework para añadir un poco de estilo a la pagina.
+de diseño.
+
+
+Estructura de la base de dades:
+
+CREATE TABLE videojuegos(
+    id bigint unsigned not null  primary key auto_increment,
+    nombre varchar(255),
+    descripcion varchar(255)
+);
---
+++ encabezado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de PHP con MySQLi by parzibyte</title>
+     <a href="http://a25zihzhazha.dam.inspedralbes.cat/videojuegotest/formulario.php"> <img src="https://www.institutpedralbes.cat/wp-content/uploads/2021/05/logo.jpg" alt="Logo Ins Pedralbes" width="165" height="60"> </a>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container-fluid">
