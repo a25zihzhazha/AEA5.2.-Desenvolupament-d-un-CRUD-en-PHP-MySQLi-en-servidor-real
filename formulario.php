57fa438ab30b7ae1eeeb6fdbf4fbe6f51a0aba0f
a25zihzhazha <a25zihzhazha@insedralbes.cat>
2026-03-23 14:22:04 +0100

    selectGenere

--- formulario.php
+++ formulario.php
@@ -11,6 +11,23 @@
                 <label for="descripcion">Descripción</label>
                 <textarea placeholder="Descripción" class="form-control" name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
             </div>
+
+            
+        //Seleccionar genero juego
+            
+            <div class="form-group">
+                <label for="genero">Gènere</label>
+            <select name="genero" id="genero">
+                <option value="accion">Acción</option>
+                <option value="survival">Survival</option>
+                <option value="lucha">Lucha</option>
+                <option value="aventura">Aventura</option>
+            </select>
+            </div>
+
+
+
+
             <div class="form-group"><button class="btn btn-success">Guardar</button></div>
         </form>
     </div>
